4370bb223e3df830201ca3d190f6af55da9ed2ac
Michael Staib <michael.staib@hotmail.com>
2019-01-31 11:33:55 +0100

    Paging example

---
+++ Paging/Startup.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using HotChocolate.AspNetCore;
+using HotChocolate;
+using HotChocolate.Execution.Configuration;
+
+namespace HotChocolate.Examples.Paging
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // If you need dependency injection with your query object add your query type as a services.
+            // services.AddSingleton<Query>();
+
+            // enable InMemory messaging services for subscription support.
+            // services.AddInMemorySubscriptionProvider();
+
+            // this enables you to use DataLoader in your resolvers.
+            services.AddDataLoaderRegistry();
+
+            // Add GraphQL Services
+            services.AddGraphQL(Schema.Create(c =>
+            {
+                // enable for authorization support
+                // c.RegisterAuthorizeDirectiveType();
+                c.RegisterQueryType<Query>();
+            }),
+            new QueryExecutionOptions { EnableTracing = true });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // enable this if you want tu support subscription.
+            // app.UseWebSockets();
+            app.UseGraphQL();
+            // enable this if you want to use graphiql instead of playground.
+            // app.UseGraphiQL();
+            app.UsePlayground();
+        }
+    }
+}

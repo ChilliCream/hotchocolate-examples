09217a041079936ea3c420f149a3007348781aec
Michael Staib <michael@chillicream.com>
2019-12-25 23:51:07 +0100

    Added pure code-first example (#3)

---
+++ PureCodeFirst/Startup.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using HotChocolate;
+using HotChocolate.AspNetCore;
+using HotChocolate.Subscriptions;
+using StarWars.Characters;
+using StarWars.Repositories;
+using StarWars.Reviews;
+
+namespace StarWars
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add the custom services like repositories etc ...
+            services.AddSingleton<ICharacterRepository, CharacterRepository>();
+            services.AddSingleton<IReviewRepository, ReviewRepository>();
+
+            // Add in-memory event provider
+            services.AddInMemorySubscriptionProvider();
+
+            // Add GraphQL Services
+            services.AddGraphQL(sp => SchemaBuilder.New()
+                .AddServices(sp)
+                .AddQueryType(d => d.Name("Query"))
+                .AddMutationType(d => d.Name("Mutation"))
+                .AddSubscriptionType(d => d.Name("Subscription"))
+                .AddType<CharacterQueries>()
+                .AddType<ReviewQueries>()
+                .AddType<ReviewMutations>()
+                .AddType<ReviewSubscriptions>()
+                .AddType<Human>()
+                .AddType<Droid>()
+                .AddType<Starship>()
+                .Create());
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app
+                .UseRouting()
+                .UseWebSockets()
+                .UseGraphQL("/graphql");
+        }
+    }
+}

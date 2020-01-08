1017ca6c24ff557f564607116888555d332e14c0
Michael Staib <michael@chillicream.com>
2020-01-08 22:05:05 +0100

    Added Code First Example

---
+++ CodeFirst/Startup.cs
@@ -0,0 +1,55 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using HotChocolate;
+using HotChocolate.AspNetCore;
+using HotChocolate.AspNetCore.Voyager;
+using HotChocolate.Execution.Configuration;
+using HotChocolate.Subscriptions;
+using StarWars.Data;
+using StarWars.Types;
+
+namespace StarWars
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add the custom services like repositories etc ...
+            services.AddSingleton<CharacterRepository>();
+            services.AddSingleton<ReviewRepository>();
+
+            // Add in-memory event provider
+            services.AddInMemorySubscriptionProvider();
+
+            // Add GraphQL Services
+            services.AddGraphQL(sp => SchemaBuilder.New()
+                .AddServices(sp)
+                .AddQueryType<QueryType>()
+                .AddMutationType<MutationType>()
+                .AddSubscriptionType<SubscriptionType>()
+                .AddType<HumanType>()
+                .AddType<DroidType>()
+                .AddType<EpisodeType>()
+                .Create());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app
+                .UseRouting()
+                .UseWebSockets()
+                .UseGraphQL("/graphql");
+        }
+    }
+}

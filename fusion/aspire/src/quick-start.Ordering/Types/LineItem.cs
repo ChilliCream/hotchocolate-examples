8fd8935679ad441842db65c53dd33af112546d2e
PascalSenn <senn.pasc@gmail.com>
2024-12-01 21:37:19 +0100

    Adds Fusion Aspire Example (#67)

---
+++ fusion/aspire/src/quick-start.Ordering/Types/LineItem.cs
@@ -0,0 +1,24 @@
+namespace quick_start.Ordering.Types;
+
+public class LineItem
+{
+    public int Id { get; set; }
+
+    public int Quantity { get; set; }
+
+    public int ProductId { get; set; }
+}
+
+[ObjectType<LineItem>]
+public static partial class LineItemType
+{
+    static partial void Configure(IObjectTypeDescriptor<LineItem> descriptor)
+    {
+        descriptor.Ignore(x => x.ProductId);
+    }
+
+    public static Product GetProduct([Parent] LineItem lineItem)
+        => new(lineItem.ProductId);
+}
+
+public sealed record Product(int Id);

d726834ca4b02a658c1212009227c8fa202ed044
Pascal Senn <pascal@chillicream.com>
2024-12-01 21:38:02 +0100

    Adds relay schema example

---
+++ fusion/relay-schema/src/quick-start.AppHost/Program.cs
@@ -0,0 +1,18 @@
+using HotChocolate.Fusion.Aspire;
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+var ordering = builder.AddProject<Projects.quick_start_Ordering>("ordering");
+var products = builder.AddProject<Projects.quick_start_Products>("products");
+
+builder
+    .AddFusionGateway<Projects.quick_start_Gateway>("gateway")
+    .WithSubgraph(ordering)
+    .WithSubgraph(products)
+    .WithOptions(new FusionCompositionOptions()
+    {
+        EnableGlobalObjectIdentification = true
+    });
+
+// Watch out. It's not just `builder.Build().Run();` anymore!
+builder.Build().Compose().Run();
